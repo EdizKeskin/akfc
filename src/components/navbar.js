b5d1dc9b4b260d6b1c6442c0ae06f415dac283ff
Ediz Keskin <edizkeskin@gmail.com>
2022-02-03 20:30:36 +0300

    Discord bağlantısı eklendi

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,40 @@
-import { Text, Box } from "@chakra-ui/react";
+import { Text, Box, Button } from "@chakra-ui/react";
 import React from "react";
+import { FaDiscord } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <Box justifyContent="center" alignItems={"center"} display="flex" py={10} flexDirection="column">
-      <Text as={"h1"} fontSize={"4xl"} fontWeight={"extrabold"} letterSpacing={"10px"} >
+    <Box
+      justifyContent="center"
+      alignItems={"center"}
+      display="flex"
+      py={10}
+      flexDirection="column"
+    >
+      <Text
+        as={"h1"}
+        fontSize={"4xl"}
+        fontWeight={"extrabold"}
+        letterSpacing={"10px"}
+      >
         AKFC
       </Text>
-      <Text as={"h1"} fontSize={"lg"} fontWeight={"medium"}>Biz anime kızlarına tapan 4 arkadaşız.</Text>
+      <Text fontSize={"lg"} fontWeight={"medium"}>
+        Biz anime kızlarına tapan 4 arkadaşız
+      </Text>
+
+      <Button
+        variant="link"
+        fontSize={"md"}
+        color={"gray.500"}
+        fontWeight={"medium"}
+        mb="-10"
+        mt="10"
+        textAlign={"center"}
+        leftIcon={<FaDiscord />}
+      >
+        <a href="https://discord.gg/VrwJWNSJfA">Bizimle takılmak istersen</a>
+      </Button>
     </Box>
   );
 }
